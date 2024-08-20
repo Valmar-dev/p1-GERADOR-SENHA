c17f9c7349e8f126a85b59427f7197b82e74fd80
Valmar-dev <valmarpontedev@gmail.com>
2024-08-20 16:12:00 -0300

    Criando estrutura do gerador

---
+++ estetica.py
@@ -0,0 +1,7 @@
+# Arquivo para adicionar cores e organizar a estrutura.
+
+# criando função para gerar títulos:
+def titulos(msg):
+    print('--' * 20)
+    print(msg.center(40))
+    print('--' * 20)
--- principal.py
+++ principal.py
@@ -1,1 +1,13 @@
-print("olá mundo!")
+#  Arquivo principal do gerador de senha
+
+import auxilio as ax
+import estetica as es
+
+# Construindo um painel pra usuário
+es.titulos("GERADOR DE SENHA")
+
+# Controlando quantidade de dígitos:
+quantidade = ax.controle()
+
+# Gerando senha:
+senha_criada = ax.gerador(quantidade)
